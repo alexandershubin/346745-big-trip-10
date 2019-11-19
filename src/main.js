5d0d7c42d438cd23730c9f9b01a68aa2dca6a0da
alexandershubin <shubin8@mail.ru>
2019-11-19 23:27:16 +0300

    поправил блок с информацией

--- src/main.js
+++ src/main.js
@@ -372,17 +372,11 @@ const createNewEventFormsTemplate = () => {
 
 const createInfoAboutWayTemplate = () => {
   return (
-    `<section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam — ... — Amsterdam</h1>
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam — ... — Amsterdam</h1>
 
-        <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;21</p>
-      </div>
-
-      <p class="trip-info__cost">
-        Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
-      </p>
-    </section>`
+      <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;21</p>
+    </div>`
   );
 };
 
