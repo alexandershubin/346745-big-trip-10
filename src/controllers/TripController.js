e719532eb923428787827b14052ca54330d2d752
alexandershubin <shubin8@mail.ru>
2020-01-07 22:56:07 +0300

    task6

--- src/controllers/TripController.js
+++ src/controllers/TripController.js
@@ -1,64 +1,53 @@
-import CardEditComponent from "../components/form-edit";
 import SortComponent from "../components/sort";
 import TripDayItemComponent from "../components/trip-day";
 import TripInfoComponent from "../components/trip-info";
-import CardComponent from "../components/card";
 import {SortType} from "../const";
-import {render, RenderPosition, replace} from "../utils/render";
+import {render, RenderPosition} from "../utils/render";
+import PointController from "./PointController";
 
 const tripEvents = document.querySelector(`.trip-events`);
 const tripInfo = document.querySelector(`.trip-main__trip-info`);
 
-const renderCards = (cards, container, isDefaultSorting = true) => {
-  const dates = isDefaultSorting ? [...new Set(cards.map((item) => new Date(item.startDate).toDateString()))]
+const renderCards = (cards, container, onDataChange, onViewChange, isDefaultSorting = true) => {
+  const pointControllers = [];
+  const dates = isDefaultSorting
+    ? [...new Set(cards.map((item) => new Date(item.startDate).toDateString()))]
     : [true];
 
   dates.forEach((date, dateIndex) => {
     const day = isDefaultSorting ? new TripDayItemComponent(new Date(date), dateIndex + 1) : new TripDayItemComponent();
-    const dayElement = day.getElement();
 
     cards.filter((card) => {
       return isDefaultSorting ? new Date(card.startDate).toDateString() === date : card;
     }).forEach((card) => {
-      const eventsList = dayElement.querySelector(`.trip-events__list`);
+      const pointController = new PointController(day.getElement().querySelector(`.trip-events__list`), onDataChange, onViewChange);
 
-      const cardComponent = new CardComponent(card);
-      const cardEditComponent = new CardEditComponent(card);
-
-      const onEscKeyDown = (evt) => {
-        const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-        if (isEscKey) {
-          replace(cardComponent, cardEditComponent);
-          document.removeEventListener(`keydown`, onEscKeyDown);
-        }
-      };
-
-      render(eventsList, cardComponent, RenderPosition.BEFOREEND);
-
-      cardComponent.setClickHandler(() => {
-        replace(cardEditComponent, cardComponent);
-        document.addEventListener(`keydown`, onEscKeyDown);
-      });
-
-      cardEditComponent.setSubmitHandler((evt) => {
-        evt.preventDefault();
-        replace(cardComponent, cardEditComponent);
-      });
+      pointController.render(card);
+      pointControllers.push(pointController);
     });
 
     render(container.getElement(), day, RenderPosition.BEFOREEND);
   });
+  return pointControllers;
 };
 
 export default class TripController {
   constructor(container) {
     this._container = container;
+
     this._eventsSortingComponent = new SortComponent();
+    this._cards = [];
+    this._showedCardControllers = [];
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onViewChange = this._onViewChange.bind(this);
   }
 
   render(cards) {
-    renderCards(cards, this._container);
+    if (this._cards.length === 0) {
+      this._cards = cards;
+    }
+    this._showedCardControllers = renderCards(cards, this._container, this._onDataChange, this._onViewChange);
 
     render(tripInfo, new TripInfoComponent(cards), RenderPosition.AFTERBEGIN);
 
@@ -69,24 +58,50 @@ export default class TripController {
       let isDefaultSorting = false;
 
       switch (sortType) {
-        case SortType.CANCEL:
+        case SortType.DATE_DOWN:
           sortedCards = cards.slice();
           isDefaultSorting = true;
           break;
-        case SortType.TIME:
+        case SortType.TIME_DOWN:
           sortedCards = cards.slice().sort((a, b) => b.startDate - a.startDate);
           break;
-        case SortType.PRICE:
+        case SortType.PRICE_DOWN:
           sortedCards = cards.slice().sort((a, b) => b.price - a.price);
           break;
       }
 
       this._container.getElement().innerHTML = ``;
-      renderCards(sortedCards, this._container, isDefaultSorting);
+      this._showedCardControllers = renderCards(
+          sortedCards,
+          this._container,
+          this._onDataChange,
+          this._onViewChange,
+          isDefaultSorting
+      );
     });
 
     const getFullPrice = cards.reduce((acc, item) => acc + item.price, 0);
 
     document.querySelector(`.trip-info__cost-value`).textContent = getFullPrice;
+  }
+
+  _onDataChange(oldCard, newCard, pointController) {
+    const index = this._cards.findIndex((card) => card === oldCard);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._cards = [
+      ...this._cards.slice(0, index),
+      newCard,
+      this._cards.slice(index + 1)
+    ];
+
+    pointController.render(newCard);
+  }
+
+  _onViewChange() {
+    this._showedCardControllers.forEach((it) => it.setDefaultView());
   }
 }
